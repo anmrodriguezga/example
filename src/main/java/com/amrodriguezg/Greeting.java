55faad42deff8d9eb398f3fb5061d99998d2e4ba
Andres Rodriguez <anmrodriguezga@unal.edu.co>
2024-07-23 14:15:54 -0500

    Added configuration properties.

--- src/main/java/com/amrodriguezg/Greeting.java
+++ src/main/java/com/amrodriguezg/Greeting.java
@@ -5,19 +5,22 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @Path("/greeting")
 public class Greeting {
     GreetingConfig config;
+    String confProp;
 
     @Inject
-    public Greeting(GreetingConfig config) {
+    public Greeting(@ConfigProperty(name = "application.greeting.recipient") String confProp, GreetingConfig config) {
         this.config = config;
+        this.confProp = confProp;
     }
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String getGreeting() {
-        return "Hello " + config.getReceipient();
+        return "Hello " + confProp + config.getReceipient();
     }
 }
